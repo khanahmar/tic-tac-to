623678fe781fef6eb32d2a362395c1d0f189e45e
Ahmar khan <ahmarbilal223344@gmail.com>
2022-12-27 14:59:45 +0500

    done

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const tiles = document.querySelectorAll(".tile");
+const tiles = Array.from(document.querySelectorAll(".tile"));
 const playerDisplay = document.querySelector(".display-player");
 const reset = document.querySelector("#reset");
 const announcer = document.querySelector(".announcer");
 
-console.log(tiles);
-console.log(Array.from(tiles));
+
 
 let board = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
@@ -14,43 +13,42 @@ const PLAYERX_WON = "PLAYERX_WON";
 const PLAYERO_WON = "PLAYERO_WON";
 const TIE = "TIE";
 
-const winningCondition = [
+const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-  [0, 1, 2],
-  [0, 3, 4],
-  [1, 4, 7],
-  [2, 5, 6],
 ];
 
 // handle result validitaion fucntion
 
 function handleResultValidation() {
-  let roundwon = false;
+  let roundWon = false;
   for (let i = 0; i <= 7; i++) {
-    const wincondition = winningCondition[i];
-    const a = board[winningCondition[0]];
-    const b = board[winningCondition[1]];
-    const c = board[winningCondition[2]];
+    const winCondition = winningConditions[i];
+    const a = board[winCondition[0]];
+    const b = board[winCondition[1]];
+    const c = board[winCondition[2]];
     if (a === "" || b === "" || c === "") {
       continue;
     }
     if (a === b && b === c) {
-      roundwon = true;
+      roundWon = true;
       break;
     }
   }
-  if (roundwon) {
+
+  if (roundWon) {
     announce(currentPlayer === "X" ? PLAYERX_WON : PLAYERO_WON);
     isGameActive = false;
     return;
   }
-  if (!board.includes("")) {
-    announce(TIE);
-  }
+
+  if (!board.includes("")) announce(TIE);
 }
 
 // upadate board function
@@ -59,7 +57,7 @@ function updateBoard(index) {
 }
 
 // announce function
-function announce(type) {
+const announce = (type) => {
   switch (type) {
     case PLAYERO_WON:
       announcer.innerHTML = `Player <span class="playerO">O</span> Won`;
@@ -70,7 +68,8 @@ function announce(type) {
     case TIE:
       announcer.innerText = "TIE";
   }
-}
+  announcer.classList.remove("hide");
+};
 
 // is valid fucntion
 function isValidAction(tile) {
@@ -95,6 +94,7 @@ function changePlayer() {
 // user acion function
 const userAction = (tile, index) => {
   if (isValidAction(tile) && isGameActive) {
+    console.log("click");
     tile.innerText = currentPlayer;
     tile.classList.add(`player${currentPlayer}`);
     updateBoard(index);
@@ -102,10 +102,9 @@ const userAction = (tile, index) => {
     changePlayer();
   }
 };
+
 // Event listner on tiles
 tiles.forEach((tile, index) => {
-  console.log(tile);
-  console.log(index);
   tile.addEventListener("click", () => userAction(tile, index));
 });
 
